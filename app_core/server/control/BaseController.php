af0ef9a40472128f3caf8dbbf996985fa6861b21
Rushpuppy <iphone@rushpuppy.ch>
2014-03-24 00:33:29 +0100

    BaseController Concept
    Prototype ideas for the BaseController

---
+++ app_core/server/control/BaseController.php
@@ -0,0 +1,32 @@
+
+<?php
+
+class BaseController {
+  // Requestdata $_GET, $_POST, $_SESSION etc.
+  public function request() {
+  }
+  
+  // Translations (ClientSide)
+  public function translate() {
+  }
+  
+  // Add File CSS, Javascript
+  public function addFile() {
+  }   
+  
+  // Add a View to Template Scope
+  public function addView() {
+  }
+  
+  // Add a Model to Model Scope
+  public function addModel() {
+  }
+  
+  // Priviledges Stuff
+  public function priviledges() {
+  }
+  
+  // Run Controller
+  public display() {
+  }
+}
